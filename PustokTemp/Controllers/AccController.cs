9151a013afbfc6ab8d561fe5429587cf94ed13df
Yusifli Enel <enelry@code.local>
2024-03-19 16:58:32 +0400

    added userRegister

---
+++ PustokTemp/Controllers/AccController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PustokTemp.DAL;
+using PustokTemp.Models;
+using PustokTemp.ViewModels;
+
+namespace PustokTemp.Controllers;
+
+public class AccController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly PustokDbContext _context;
+
+    public AccController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<AppUser> signInManager,PustokDbContext context)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _signInManager = signInManager;
+        _context = context;
+    }
+    public IActionResult Register()
+    {
+        return View();
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(UserRegisterViewModel userRegisterViewModel)
+    {
+        if (!ModelState.IsValid)return View();
+        AppUser member = new AppUser()
+        {
+            FullName = userRegisterViewModel.FullName,
+            UserName = userRegisterViewModel.UserName,
+            Email = userRegisterViewModel.Email
+        };
+        if(await _context.Users.AnyAsync(x => x.NormalizedUserName == member.UserName))
+        {
+            ModelState.AddModelError("UserName", "UserName is already exist");
+            return View();
+        }
+        if (await _context.Users.AnyAsync(x => x.NormalizedEmail == member.Email))
+        {
+            ModelState.AddModelError("Email", "Email is already exist");
+            return View();
+        }
+        var result= await _userManager.CreateAsync(member, userRegisterViewModel.Password);
+        if (!result.Succeeded)
+        {
+            foreach (var err in result.Errors) { 
+            ModelState.AddModelError("",err.Description);
+            }
+            return View();
+        }
+        var roleResult = await _userManager.AddToRoleAsync(member, "Member");
+        if (!roleResult.Succeeded)
+        {
+            foreach (var err in roleResult.Errors)
+            {
+                ModelState.AddModelError("", err.Description);
+            }
+            return View();
+        }
+        return RedirectToAction("index","home");
+    }
+
+}
--- PustokTemp/obj/Debug/net8.0/PustokTemp.AssemblyInfo.cs
+++ PustokTemp/obj/Debug/net8.0/PustokTemp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PustokTemp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b349847ec0a43a12e9866219ae56989d4e2dd70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee6a3c924acd7681f7ef732677b7cd85727ef61")]
 [assembly: System.Reflection.AssemblyProductAttribute("PustokTemp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PustokTemp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
